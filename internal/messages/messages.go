038e8721d927ab0fd61ea957f42200b7d57a7a77
Don2Quixote <idonquixote@yandex.ru>
2022-01-07 21:35:10 +0300

    Chagned log message

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -95,7 +95,7 @@ func (m *Messages) Send(ctx context.Context, id int64, message entity.OutgoingMe
 
 	_, err := m.bot.Send(tgMessage)
 	if err != nil {
-		return errors.Wrap(err, "can't send message")
+		return errors.Wrap(err, "can't send telegram message")
 	}
 
 	return nil
